fa5e306da5f7e1b63b0dd5af89f7352be62d8237
Jackson Golden <jackson.golden@gmail.com>
2020-10-28 10:22:21 -0400

    Created IOUtility class to handle general I/O.

---
+++ CIT591_HW05_SpellChecker/src/IOUtility.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.io.*;
+
+/**
+ * A class that has I/O methods used by both {@link WordRecommender} and {@link WordRecommenderRunner}.
+ * @author Jackson Golden
+ */
+public class IOUtility {
+
+	// INSTANCE VARIABLES
+	
+	// CONSTRUCTORS
+
+	// METHODS
+
+}
