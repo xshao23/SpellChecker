0797beed16b6cc7e51c46542d90aabe8612c5f81
Jackson Golden <jackson.golden@gmail.com>
2020-10-28 11:28:34 -0400

    Prepared for-loop to look through WordRecommender dictionary.

--- CIT591_HW05_SpellChecker/src/WordRecommenderRunner.java
+++ CIT591_HW05_SpellChecker/src/WordRecommenderRunner.java
@@ -35,41 +35,51 @@ public class WordRecommenderRunner {
 	public static void main(String[] args) {
 		
 		// Initialize objects.
+		// - WordRecommender object, selecting the dictionary file.
+		WordRecommender wr = new WordRecommender("shortEngDictionary.txt");
 		// - WordRecommenderRunner object for use, selecting the dictionary for WordRecommender to use.
-		WordRecommenderRunner runner = new WordRecommenderRunner(new WordRecommender("shortEngDictionary.txt"));		
+		WordRecommenderRunner runner = new WordRecommenderRunner(wr);		
 		// - Create scanner object for user input.
 		Scanner userInputScanner = new Scanner(System.in);
 		// - IOUtility object to handle I/O.
 		IOUtility ioHelper = new IOUtility();
 
-
 		// Welcome user to program.
 		System.out.println("Welcome!");
 		
 		// Get file object to be spell-checked.
+		System.out.println("What file would you like to spell-check?");
 		File inputFile = ioHelper.getTxtFileFromUser(userInputScanner);
+		System.out.println("Reviewing words in " + inputFile.getName() + "...");
 		
 		// Read in file to ArrayList<String> of words in the file.
 		ArrayList<String> inputWords = ioHelper.readFileToList(inputFile);
 		
 		// Create new ArrayList<String> of words for output.
-		ArrayList<String> outputWords = new ArrayList<String>();
+		ArrayList<String> outputWords = new ArrayList<String>();		
 		
-		// Ask user for "N" in "topN" (and explain). Loop until input is valid.
-		
-		System.out.println("Reviewing words in " + inputFile.getName() + "...");
 		// For each word in inputWords:
 		for (String inputWord : inputWords) {
+
 			// Check if inputWord is in wr.filename (dictionary).
+			if (wr.wordsInDict.contains(inputWord)) {
 			
-			// If it is, place in outputWords and continue.
-			outputWords.add(inputWord);
-			// If it is not, then:		
-			//     i) Call WordRecommender.getWordSuggestions
-			//    ii) Prompt for user input; loop until input is valid.
-			//   iii) Update input words with result of user input. (Maybe make ALL CAPS if updated?)
+				// If it is, place in outputWords and continue.
+				outputWords.add(inputWord);
+				continue;
+				
+			}
+			
+			// If it is not, then:
+			else {
+				//     i) Call WordRecommender.getWordSuggestions
+				//    ii) Prompt for user input; loop until input is valid.
+				//   iii) Update input words with result of user input. (Maybe make ALL CAPS if updated?)
+				continue;
+			}
 			
 		}
+
 		System.out.println("Review complete!");
 		
 		// Get name of output file
